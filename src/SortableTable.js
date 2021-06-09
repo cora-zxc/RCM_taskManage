90c1cfe4a35147ddb36d00fde251b006afc85380
cora-zxc <free0610on@gmail.com>
2021-06-09 17:31:43 +0800

    20210609_01

--- src/SortableTable.js
+++ src/SortableTable.js
@@ -6,6 +6,9 @@ import arrayMove from 'array-move';
 import { sortableHandle } from 'react-sortable-hoc';
 import { MenuOutlined } from '@ant-design/icons';
 import ManuallyEdit from './ManuallyEdit';
+import InputFiles from 'react-input-files';
+import XLSX from 'xlsx';
+import './index.css';
 
 const SortableItem = sortableElement(props => <tr {...props} />);
 const SortableContainer = sortableContainer(props => <tbody {...props} />);
@@ -15,29 +18,99 @@ class SortableTable extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			data: this.props.data
+			data: []
 		};
 	}
+	getExcel = (excel) => {
+        //8碼日期y+m+d
+		let date = new Date();
+		var y = date.getFullYear().toString();
+		var m = (date.getMonth()+1).toString();
+			if(m < 10){
+				m = "0" + m;
+			}
+		var d = date.getDate().toString();
+            if (d < 10){
+                d = "0" + d ;
+            }
+        //欄位資料的建構式
+        function MachineInfo(index,taskId,produceBatchNo,productModelNo,currentQuantity,machinePosition){
+            return{
+                index : index,
+                taskId : taskId,
+                produceBatchNo : produceBatchNo,
+                productModelNo : productModelNo,
+                currentQuantity : currentQuantity,
+                machinePosition : machinePosition,
+                key : '',
+                action : '',
+                currentStatus : '未派工',
+                eRackPositionNotTested : '',
+                eRackPositionTested : '',
+                pv : '50'
+            };
+        }
+        //迴圈取資料
+        var data = [];
+		var i = 0,j,k;
+        var m_index = 0;
+        var m_taskId,m_produceBatchNo,m_productModelNo,m_currentQuantity,m_machinePosition;
+		for(;excel.length > i; ++i){
+			if(excel[i].hasOwnProperty('矽格股份有限公司')){
+				if(excel[i]['矽格股份有限公司'].hasOwnProperty('length')){  
+					if(excel[i]['矽格股份有限公司'].indexOf('DX-') > -1){
+						m_machinePosition = excel[i]['矽格股份有限公司'];
+						for(j = i + 2,k = 0;4 > k && excel.length > j && excel[j].hasOwnProperty('__EMPTY_2');++j,++k){
+							m_taskId =  y + m + d + excel[j].__EMPTY_2;
+							m_produceBatchNo = excel[j].__EMPTY_2;
+							m_productModelNo = excel[j].__EMPTY_4;
+							m_currentQuantity = excel[j].__EMPTY_7;
+							data[m_index] = MachineInfo(m_index,m_taskId,m_produceBatchNo,m_productModelNo,m_currentQuantity,m_machinePosition);
+							++m_index;
+						}
+						if(excel[i].hasOwnProperty('__EMPTY_12')){
+							m_machinePosition = excel[i].__EMPTY_12;
+							for(j = i + 2,k = 0;4 > k && excel.length > j && excel[j].hasOwnProperty('__EMPTY_15');++j,++k){
+								m_taskId =  y + m + d + excel[j].__EMPTY_15;
+								m_produceBatchNo = excel[j].__EMPTY_15;
+								m_productModelNo = excel[j].__EMPTY_17;
+								m_currentQuantity = excel[j].__EMPTY_20;
+								data[m_index] = MachineInfo(m_index,m_taskId,m_produceBatchNo,m_productModelNo,m_currentQuantity,m_machinePosition);
+								++m_index;
+							}
+						}
+					}
+				}
+			}
+		}
+		return data;
+	}
+    onImportExcel = files => {
+        const fileReader = new FileReader();
+        for (let index = 0; index < files.length; index++)
+            fileReader.name = files[index].name;
+        fileReader.onload = event => {
+            try {
+                const fileExt = event.target.name;
+                if (fileExt == null)
+                    throw RangeError("檔案為空值");
+                const { result } = event.target;
+                const workbook = XLSX.read(result, { type: "binary" });
+                let excel = [];
+                excel = excel.concat(
+                    XLSX.utils.sheet_to_json(workbook.Sheets['DX'])
+                );
+				this.setState({data: this.getExcel(excel)});
+            } catch (e) {
+                alert(e);
+                return;
+            }
+        };
+        fileReader.readAsBinaryString(files[0]);
+    };
 	delete = () => {
 		console.log("已刪除");
 	  };
-	// changeOrderStatus = (event) => {
-	// 	console.log(event);
-	// 	event.target.parentNode.parentNode.nextSibling.textContent = "已派工";
-	// 	event.target.parentNode.parentNode.nextSibling.style.color = "blue";
-	// };
-	// changeToDeleteStatus = (event) => {
-	// 	var status = window.confirm("確定要刪除嗎？");
-	// 	if(status){
-	// 		console.log(event.target.parentNode.parentNode);
-	// 		event.target.parentNode.parentNode.nextSibling.textContent = "已刪除";
-	// 		event.target.parentNode.parentNode.nextSibling.style.color = "red";
-	// 		event.target.disabled=true;
-	// 		event.target.previousSibling.disabled=true;
-	// 		event.target.nextSibling.lastChild.disabled=true;
-	// 	}
-	// 	//console.log(event);
-	//}
 	onSortEnd = ({ oldIndex, newIndex }) => {
 		const { data } = this.state;
 	  	if (oldIndex !== newIndex){
@@ -60,7 +133,6 @@ class SortableTable extends React.Component {
 		return <SortableItem index={index} {...restProps} />;
 	}
 	render() {
-		const { data } = this.state;
 		this.columns = [
 			{
 			  title: "拖拉",
@@ -104,7 +176,6 @@ class SortableTable extends React.Component {
 					<Button
 					className="ant-btn"
 					type="button"
-					// onClick={this.changeOrderStatus}
 					>
 					派
 					</Button>
@@ -142,20 +213,29 @@ class SortableTable extends React.Component {
 			}
 		  ];
 		return (
-			<Table
-				pagination={false} //true -> 分頁
-				columns={this.columns}
-				dataSource={data}
-				rowKey="index"
-				components={
-					{
-						body: {
-							wrapper: this.DraggableContainer,
-							row: this.DraggableBodyRow,
-						},
+			<div>
+				<span>
+					<InputFiles accept=".xlsx, .xls" onChange={this.onImportExcel}>
+						<Button className="extractButton" type="primary">
+							匯入Excel
+						</Button>
+					</InputFiles>
+				</span>
+				<Table
+					pagination={false}
+					columns={this.columns}
+					dataSource={this.state.data}
+					rowKey="index"
+					components={
+						{
+							body: {
+								wrapper: this.DraggableContainer,
+								row: this.DraggableBodyRow,
+							},
+						}
 					}
-				}
-			/>
+				/>
+			</div>
 		);
 	};
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import InputFile from './InputFile';
 import ManuallyCreate from './ManuallyCreate';
 import SortableTable from './SortableTable';
 
 ReactDOM.render(
   <div>
-    <InputFile accept=".xlsx, .xls" name ="匯入Excel" />
     <ManuallyCreate name="手動新增" />
-    <SortableTable data={[]}/>
+    <SortableTable />
   </div>,
   document.getElementById('root')
 );
